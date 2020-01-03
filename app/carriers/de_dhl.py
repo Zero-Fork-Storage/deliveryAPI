e5dd26d8b28aeb9b34e7bd9fee9bebc9a1045cc8
zeroday0619 <zeroday0619@zeroday0619.kr>
2020-01-03 10:32:36 +0900

    2%

--- app/carriers/de_dhl.py
+++ app/carriers/de_dhl.py
@@ -12,8 +12,8 @@ class de_dhl:
             'picked up': {'id': 'at_pickup', 'text': '상품인수'},
             'delivered': {'id': 'delivered', 'text': '배송완료'}
         }
-    def getTime(self, location, date, time):
-        dateObj = datetime.
+    #def getTime(self, location, date, time):
+        
 
 
 
@@ -23,13 +23,13 @@ class de_dhl:
             params={
                 'AWB': track_id,
                 'countryCode': 'g0',
-                'languageCode': 'en'
+                'languageCode': 'ko'
             },
             headers={
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
             }
         )
-        print(resp.status_code)
+        #print(resp.status_code)
         if resp.status_code != 200:
             print("ERROR")
             return
@@ -40,7 +40,11 @@ class de_dhl:
         data = de_dhl().query(track_id).json()
         results = data['results']
         info = results[0]
-
+        #print(json.dumps(info,indent=4, sort_keys=True, ensure_ascii=False))
+        checkpoints = info['checkpoints']
+        checkpoints_len = len(info['checkpoints'])
+        last_push = (checkpoints_len - 1 - (checkpoints_len - 1))
+        # print(checkpoints[last_push])
         shippingInfo = {
             'from': {
                 'name': info['origin']['value']
@@ -48,13 +52,14 @@ class de_dhl:
             'to': {
                 'name': info['destination']['value']
             },
-            'state': { 'id': 'information_received', 'text':'접수완료' },
-            'progresses': []
+            'state': {
+                'info': checkpoints[last_push]
+            }
         }
         return shippingInfo
 
 app = de_dhl()
-print(app.get("7706689793"))
+#print(json.dumps(app.get("-------------"),indent=4, sort_keys=False, ensure_ascii=False))
 
         
         
