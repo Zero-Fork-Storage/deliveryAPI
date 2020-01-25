d9c76aec96013fae89a583df89aed25cb9e751a7
zeroday0619 <zeroday0619@zeroday0619.kr>
2020-01-26 00:55:36 +0900

    run ----> main

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from app import tracking_bot
+app = Flask(__name__)
+app.register_blueprint(tracking_bot)
+
+@app.route('/', methods=['GET'])
+def HelloApi():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=80)
