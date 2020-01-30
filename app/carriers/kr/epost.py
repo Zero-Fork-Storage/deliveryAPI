3990a026ed2a51f360a2fdb4039b400b104f4ab4
zeroday0619 <zeroday0619@kakao.com>
2020-01-30 14:24:37 +0900

    epost progressTableResult 70%

--- app/carriers/kr/epost.py
+++ app/carriers/kr/epost.py
@@ -18,7 +18,7 @@ resp = requests.post(
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
     },
     params={
-        'sid1': '6066252805008'
+        'sid1': ''
     }
 )
 
@@ -31,8 +31,20 @@ for row in informationTable.find_all('td'):
     array.append(nospace)
 array = ' '.join(array).split()
 print(array)
-    
+def editList(l, n):
+    for i in range(0, len(l)-5, n):
+        yield l[i:i + n]
 
+progressTable = soup.select('.table_col:nth-child(1)')[0]
+# print(progressTable)
+progressTableArray = []
+for row3 in progressTable.find_all('td'):
+    row4 = re.sub('<.+?>', '',str(row3)).strip()
+    nospace2 = re.sub('&nbsp;| |\t|\r|', '',row4).replace('\n', ' ')
+    nospace2 = re.sub('\([^)]*\)', '', nospace2)
+    progressTableArray.append(nospace2)
+progressTableArray = ' '.join(progressTableArray).strip().split()
 
-progressTable = soup.select('.table_col:nth-child(1)')
-#print(progressTable)
+progressTableResult = list(editList(progressTableArray, 4))
+print(progressTableResult)
+
