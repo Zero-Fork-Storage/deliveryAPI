69fdd93257bd3e9ce0ce3282b47eebbab1bc75db
zeroday0619 <zeroday0619@kakao.com>
2020-03-22 15:34:47 +0900

    first commit

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM tiangolo/uwsgi-nginx-flask:python3.7
-LABEL maintainer="zeroday0619 <zeroday0619@zeroday0619.kr>"
-COPY . /deliveryApi
-WORKDIR /deliveryApi
-RUN pip3 install -r requirements.txt
-ENTRYPOINT ["python3"]
-CMD ["main.py"]
--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from flask_restful import Api
-from app.home import DeliveryApiRouter
-
-tracking_bot = Blueprint('tracking_bot', __name__)
-api = Api(tracking_bot)
-
-api.add_resource(DeliveryApiRouter, "/v1/tracking")
